e750742f8b05a600f247b0f866a10e41d0a2128c
Frederic Lemoine <fredericlemoine@users.noreply.github.com>
2019-07-18 15:25:57 +0200

    Added Float64RangeF

--- unif.go
+++ unif.go
@@ -8,6 +8,10 @@ func Float64Range(a, b int) float64 {
 	return float64(a) + rand.Float64()*float64(b-a)
 }
 
+func Float64RangeF(a, b float64) float64 {
+	return a + rand.Float64()*(b-a)
+}
+
 func Proba(p float64) bool {
 	return (rand.Float64() < p)
 }
